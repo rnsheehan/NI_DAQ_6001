c70895d37e325ea8dc0a2dc790a4862abd0bd353
Robert Sheehan <robertnsheehan@gmail.com>
2025-11-21 15:39:21 +0000

    Testing
    Initial testing of NI-DAQ USB 6001 completed. All working well. Should be able to deploy this quite easily.

--- NI_DAQ_6001/NI_DAQ_6001/NI_DAQ_6001.py
+++ NI_DAQ_6001/NI_DAQ_6001/NI_DAQ_6001.py
@@ -9,11 +9,167 @@ import pyvisa
 import time
 import numpy
 import math
+import nidaqmx
 
-# The aim of this script is to establish comms with the Chilas TLS
-# R. Sheehan 13 - 11 - 2025
+# The aim of this script is to establish comms with NI-DAQ USB 6001
+# Documentation online: https://github.com/ni/nidaqmx-python/tree/master
+# R. Sheehan 21 - 11 - 2025
 
 MOD_NAME_STR = "NI_DAQ_600x"
+
+def AO_Write_Test():
+
+    # check that your NI-DAQ is being controlled by Python by getting AO channel to write an output
+    # R. Sheehan 21 - 11 - 2025
+
+    FUNC_NAME = ".AO_Write_Test()" # use this in exception handling messages
+    ERR_STATEMENT = "Error: " + MOD_NAME_STR + FUNC_NAME
+    
+    try:
+        task = nidaqmx.Task()
+        
+        task.ao_channels.add_ao_voltage_chan('Dev1/ao0')
+        
+        task.start()
+        
+        voltage = 2.7        
+        task.write(voltage)
+        time.sleep(5)
+        
+        voltage = -4.2
+        task.write(voltage)
+        time.sleep(5)
+        
+        voltage = 0.0
+        task.write(voltage)
+        time.sleep(5)
+
+        task.stop()
+        
+        task.close()
+
+    except Exception as e:
+        print(ERR_STATEMENT)
+        print(e)
+        
+def AI_Read_Test():
+    
+    # check that your NI-DAQ is being controlled by Python by getting AI channel to read an output
+    # R. Sheehan 21 - 11 - 2025
+
+    FUNC_NAME = ".AI_Read_Test()" # use this in exception handling messages
+    ERR_STATEMENT = "Error: " + MOD_NAME_STR + FUNC_NAME
+
+    try:
+        task = nidaqmx.Task()
+        
+        task.ai_channels.add_ai_voltage_chan('Dev1/ai0')
+        
+        task.start()
+        
+        N = 61
+        count = 0
+        while count < N:
+            value = task.read()
+            print(value)
+            time.sleep(1)
+            count = count + 1
+
+        task.stop()
+        
+        task.close()
+
+    except Exception as e:
+        print(ERR_STATEMENT)
+        print(e)
+
+def AO_AI_Loopback_Test():
+    
+    # check that your NI-DAQ is being controlled by Python
+    # write an AO value
+    # read the same voltage value using AI
+    # R. Sheehan 21 - 11 - 2025
+
+    FUNC_NAME = ".AO_AI_Loopback_Test()" # use this in exception handling messages
+    ERR_STATEMENT = "Error: " + MOD_NAME_STR + FUNC_NAME
+
+    try:
+        # Configure Analog Output
+        ao_task = nidaqmx.Task()
+        ao_task.ao_channels.add_ao_voltage_chan('Dev1/ao0', min_val = -10, max_val = +10)
+        ao_task.start()
+        
+        # Configure Analog Input
+        from nidaqmx.constants import (TerminalConfiguration)
+        ai_task = nidaqmx.Task()        
+        ai_task.ai_channels.add_ai_voltage_chan('Dev1/ai0',terminal_config=TerminalConfiguration.DIFF, min_val = -10, max_val = +10)        
+        #ai_task.ai_channels.add_ai_voltage_chan('Dev1/ai0')        
+        ai_task.start()
+        
+        # output the voltage value
+        voltage = 7.6234
+        ao_task.write(voltage)   
+        print("Set voltage:",voltage," (V)")
+        
+        # read some data
+        N = 21
+        count = 0
+        read_vals = numpy.array([]) # instantiate an empty numpy array
+        while count < N:
+            value = ai_task.read()
+            read_vals = numpy.append(read_vals, value)
+            time.sleep(0.5)
+            count = count + 1
+        avg = numpy.mean(read_vals)
+        stdev = numpy.std(read_vals, ddof = 1)
+        upper = avg+stdev
+        lower = avg-stdev
+        in_range = True if voltage < upper and voltage > lower else False
+        print("Average of ",N,"reads: %(v1)0.04f +/- %(v2)0.04f (V)"%{"v1":avg, "v2":stdev})
+        print("Upper bound: %(v1)0.04f (V)"%{"v1":upper})
+        print("Lower bound: %(v1)0.04f (V)"%{"v1":lower})
+        if in_range:
+            print("Output is accurate to mV level")    
+        
+        # change the voltage value
+        voltage = -4.3271       
+        ao_task.write(voltage)
+        print("\nSet voltage:",voltage," (V)")
+        
+        # read some more data
+        N = 21
+        count = 0
+        read_vals = numpy.array([]) # instantiate an empty numpy array
+        while count < N:
+            value = ai_task.read()
+            read_vals = numpy.append(read_vals, value)
+            time.sleep(0.5)
+            count = count + 1
+        avg = numpy.mean(read_vals)
+        stdev = numpy.std(read_vals, ddof = 1)
+        upper = avg+stdev
+        lower = avg-stdev
+        in_range = True if voltage < upper and voltage > lower else False
+        print("Average of ",N,"reads: %(v1)0.04f +/- %(v2)0.04f (V)"%{"v1":avg, "v2":stdev})
+        print("Upper bound: %(v1)0.04f (V)"%{"v1":upper})
+        print("Lower bound: %(v1)0.04f (V)"%{"v1":lower})
+        if in_range:
+            print("Output is accurate to mV level")    
+        
+        # reset to zero
+        voltage = 0.0        
+        ao_task.write(voltage)    
+        
+        # close all tasks
+        ao_task.stop()
+        ai_task.stop()
+        
+        ao_task.close()
+        ai_task.close()
+
+    except Exception as e:
+        print(ERR_STATEMENT)
+        print(e)
 
 def main():
     pass
@@ -24,3 +180,9 @@ if __name__ == '__main__':
     pwd = os.getcwd() # get current working directory
 
     print(pwd)
+    
+    #AO_Write_Test()
+    
+    #AI_Read_Test()
+    
+    AO_AI_Loopback_Test()
