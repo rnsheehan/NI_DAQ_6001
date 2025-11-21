1f2038f11aaace6c70e639bc0fd55cb40cd2aeac
Robert Sheehan <robertnsheehan@gmail.com>
2025-11-21 14:38:32 +0000

    Proj Start
    Started the project

---
+++ NI_DAQ_6001/NI_DAQ_6001/NI_DAQ_6001.py
@@ -0,0 +1,26 @@
+# Import various modules
+
+import os
+import sys
+import glob
+import re
+import serial
+import pyvisa
+import time
+import numpy
+import math
+
+# The aim of this script is to establish comms with the Chilas TLS
+# R. Sheehan 13 - 11 - 2025
+
+MOD_NAME_STR = "NI_DAQ_600x"
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+
+    pwd = os.getcwd() # get current working directory
+
+    print(pwd)
