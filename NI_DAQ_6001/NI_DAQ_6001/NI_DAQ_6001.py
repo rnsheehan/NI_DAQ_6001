c0601fcd77aabb173251295fb9a6bc1e8c00c57c
Robert Sheehan <robertnsheehan@gmail.com>
2025-11-25 20:41:06 +0000

    NI-DAQ Coding
    String processing to determine how to allocate the sample rate correctly. Some cases worked out, need to work on others. Could take the easy way out and just restrict the channel choices available, but where's the fun in that.

--- NI_DAQ_6001/NI_DAQ_6001/NI_DAQ_6001.py
+++ NI_DAQ_6001/NI_DAQ_6001/NI_DAQ_6001.py
@@ -12,9 +12,12 @@ import time
 import numpy
 import math
 import nidaqmx
+import Common
+import Plotting
 
 # The aim of this script is to establish comms with NI-DAQ USB 6001
 # Official Documentation: https://nidaqmx-python.readthedocs.io/en/stable/
+# Examples with explanation: https://nidaqmx-python.readthedocs.io/en/stable/#python-examples
 # Nice introduction: https://www.halvorsen.blog/documents/programming/python/resources/powerpoints/DAQ%20with%20Python.pdf
 # Examples online: https://github.com/ni/nidaqmx-python/tree/master
 # R. Sheehan 21 - 11 - 2025
@@ -185,7 +188,7 @@ def AI_Read_Multiple_Channels():
     ERR_STATEMENT = "Error: " + MOD_NAME_STR + FUNC_NAME
 
     try:
-        from nidaqmx.constants import (TerminalConfiguration)
+        from nidaqmx.constants import TerminalConfiguration, AcquisitionType, READ_ALL_AVAILABLE
 
         AI_SR_MAX = 20000 # max sample rate on single AI channel, units of Hz
         AO_SR_MAX = 5000 # max sample rate on single AO channel, units of Hz
@@ -200,13 +203,15 @@ def AI_Read_Multiple_Channels():
         ao_task = nidaqmx.Task()
         ao_chn_str = 'Dev2/ao0:1'
         ao_task.ao_channels.add_ao_voltage_chan(ao_chn_str, min_val = -10, max_val = +10)
+        sample_rate = AO_SR_MAX / 2; 
+        ao_task.timing.cfg_samp_clk_timing(sample_rate, sample_mode = AcquisitionType.FINITE, no_samples = 500)
         ao_task.start()
         
         # Configure Analog Input
         from nidaqmx.constants import (TerminalConfiguration)
         ai_task = nidaqmx.Task()        
         ai_chn_str = 'Dev2/ai0:3'
-        ai_task.ai_channels.add_ai_voltage_chan(ai_chn_str, terminal_config=TerminalConfiguration.DIFF, min_val = -10, max_val = +10)               
+        ai_task.ai_channels.add_ai_voltage_chan(ai_chn_str, terminal_config=TerminalConfiguration.DIFF, min_val = -10, max_val = +10)
         ai_task.start()
         
         # output the voltage value
@@ -240,6 +245,92 @@ def AI_Read_Multiple_Channels():
         print(ERR_STATEMENT)
         print(e)
 
+def Bitwise_Operator_Hacking():
+
+    # Playing with different methods for determining the numbers of channels connected
+
+    AI_SR_MAX = 20000 # max sample rate on single AI channel, units of Hz
+    AO_SR_MAX = 5000 # max sample rate on single AO channel, units of Hz
+
+    # Bitwise operators, because why not
+    # https://wiki.python.org/moin/BitwiseOperators
+    print("The Number: ",AO_SR_MAX)
+    print("Divide by 2: ",AO_SR_MAX>>1)
+    print("Divide by 4: ",AO_SR_MAX>>2)
+    print("Divide by 8: ",AO_SR_MAX>>3)
+    print("Multiply by 2: ",AO_SR_MAX<<1)
+    print("Multiply by 4: ",AO_SR_MAX<<2)
+    print("Multiply by 8: ",AO_SR_MAX<<3)
+    print("\nThe Number: ",AI_SR_MAX)
+    print("Divide by 2: ",AI_SR_MAX>>1)
+    print("Divide by 4: ",AI_SR_MAX>>2)
+    print("Divide by 8: ",AI_SR_MAX>>3)
+    print("Multiply by 2: ",AI_SR_MAX<<1)
+    print("Multiply by 4: ",AI_SR_MAX<<2)
+    print("Multiply by 8: ",AI_SR_MAX<<3)
+
+def NI_DAQ_String_Hacking():
+    
+    # Playing with different methods for determining the numbers of channels connected
+    # Various methods for representing the channel naming string
+    # ai_chn_str = 'Dev2/ai0:x', x = 1, 2, 3, 4, 5, 6, 7 to open all channels in single-ended mode
+    # ai_chn_str = 'Dev2/ai0:x', x = 1, 2, 3 to open all channels in differential mode
+    # The following is also possible
+    # ai_chn_str = 'Dev2/ai0, Dev2/ai1, Dev2/ai4, Dev2/ai7'
+    # Need to count the number of channels being accessed
+
+    AI_SR_MAX = 20000 # max sample rate on single AI channel, units of Hz
+    AO_SR_MAX = 5000 # max sample rate on single AO channel, units of Hz
+
+    # No commas, single semi-colon
+    ai_chn_str = 'Dev2/ai0:7'
+    print()
+    print(ai_chn_str)
+    #print(int(ai_chn_str.split(':')[-1]))    
+    #print(ai_chn_str.replace('Dev2/',''))
+    #print( Common.extract_values_from_string( ai_chn_str.replace( 'Dev2/', '' ) ) )
+    #print( list ( map ( int, Common.extract_values_from_string( ai_chn_str.replace( 'Dev2/', '' ) ) ) ) ) 
+    #print( max ( list ( map ( int, Common.extract_values_from_string( ai_chn_str.replace( 'Dev2/', '' ) ) ) ) ) )
+    print("No. channels: ", 1 + max ( list ( map ( int, Common.extract_values_from_string( ai_chn_str.replace( 'Dev2/', '' ) ) ) ) ) )
+    print("Sample Rate per channel: ", AI_SR_MAX / ( 1 + max ( list ( map ( int, Common.extract_values_from_string( ai_chn_str.replace( 'Dev2/', '' ) ) ) ) ) ) )
+
+    # No semi-colons, multiple commas
+    ai_chn_str = 'Dev2/ai0, Dev2/ai1, Dev2/ai4, Dev2/ai7'
+    print()
+    print(ai_chn_str)
+    #print(ai_chn_str.replace('Dev2/',''))
+    #print( Common.extract_values_from_string( ai_chn_str.replace( 'Dev2/', '' ) ) )
+    #print( list ( map ( int, Common.extract_values_from_string( ai_chn_str.replace( 'Dev2/', '' ) ) ) ) ) 
+    #print( len ( list ( map ( int, Common.extract_values_from_string( ai_chn_str.replace( 'Dev2/', '' ) ) ) ) ) )
+    print("No. channels: ", len ( list ( map ( int, Common.extract_values_from_string( ai_chn_str.replace( 'Dev2/', '' ) ) ) ) ) )
+    print("Sample Rate per channel: ", AI_SR_MAX / ( len ( list ( map ( int, Common.extract_values_from_string( ai_chn_str.replace( 'Dev2/', '' ) ) ) ) ) ) )
+
+    # This works but how do you handle general cases like the one below? 
+    # mix semi-colon, commas
+
+    # re.findall(r"[-+]?\d+[\.]?\d*", the_string)
+
+    ai_chn_str = 'Dev2/ai0:3, Dev2/ai4, Dev2/ai6'
+    print()
+    print(ai_chn_str)
+    print(ai_chn_str.replace('Dev2/','') )
+    print( re.findall(r"[-+]?\d+[\.]?\d*", ai_chn_str.replace( 'Dev2/', '' ) ) )
+    print( list ( map ( int, re.findall(r"[-+]?\d+[\.]?\d*", ai_chn_str.replace( 'Dev2/', '' ) ) ) ) ) 
+
+    ai_chn_str = 'Dev2/ai0:2, Dev2/ai5:7'
+    print()
+    print(ai_chn_str)
+    print(ai_chn_str.replace('Dev2/','') )
+    print( re.findall(r"[-+]?\d+[\.]?\d*", ai_chn_str.replace( 'Dev2/', '' ) ) )
+    print( list ( map ( int, re.findall(r"[-+]?\d+[\.]?\d*", ai_chn_str.replace( 'Dev2/', '' ) ) ) ) )
+    
+    ai_chn_str = 'Dev2/ai3:6'
+    print()
+    print(ai_chn_str)
+    print(ai_chn_str.replace('Dev2/','') )
+    print( re.findall(r"[-+]?\d+[\.]?\d*", ai_chn_str.replace( 'Dev2/', '' ) ) )
+    print( list ( map ( int, re.findall(r"[-+]?\d+[\.]?\d*", ai_chn_str.replace( 'Dev2/', '' ) ) ) ) )
+
 def main():
     pass
 
@@ -256,4 +347,6 @@ if __name__ == '__main__':
     
     #AO_AI_Loopback_Test()
 
-    AI_Read_Multiple_Channels()
+    #AI_Read_Multiple_Channels()
+
+    NI_DAQ_String_Hacking()
